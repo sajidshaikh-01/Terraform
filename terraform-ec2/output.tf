4133877301cc12ceacb6f941e951e4cc8fac7813
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-16 17:26:58 +0530

    Create output.tf

---
+++ terraform-ec2/output.tf
@@ -0,0 +1,11 @@
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.ec2.public_dns
+}
+
+output "sg_id" {
+  value = aws_security_group.ec2_sg
+}
