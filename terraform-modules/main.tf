907f241653f334aad3dd9f612d3bc38b996e0404
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-17 12:33:45 +0530

    Update main.tf

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -1,1 +1,11 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "simple_ec2" {
+  source        = "./modules/ec2"
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  name          = "my-first-module-ec2"
+}
