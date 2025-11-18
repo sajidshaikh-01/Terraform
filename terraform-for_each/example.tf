e020fc1b0633a77d32bf98926c0e14a80a3158cc
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-19 01:38:14 +0530

    Update example.tf

--- terraform-for_each/example.tf
+++ terraform-for_each/example.tf
@@ -1,1 +1,15 @@
+variable "instance_names" {
+  default = ["web", "app", "db"]
+}
+
+resource "aws_instance" "ec2" {
+  for_each = toset(var.instance_names)
+
+  ami           = "ami-04a37924ffe27da53"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = each.key
+  }
+}
 
