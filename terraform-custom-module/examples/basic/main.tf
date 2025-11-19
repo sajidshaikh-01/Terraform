764a1902b8505c015e19450e6877d9c2615957ab
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-19 22:00:52 +0530

    Update main.tf

--- terraform-custom-module/examples/basic/main.tf
+++ terraform-custom-module/examples/basic/main.tf
@@ -1,1 +1,15 @@
+module "demo" {
+  source = "../.."
+
+  name            = "demo-ec2"
+  ami_id          = "ami-04a37924ffe27da53"
+  instance_type   = "t2.micro"
+  subnet_id       = "subnet-12345"
+  key_name        = "mykey"
+  security_groups = ["sg-123"]
+
+  tags = {
+    Owner = "Sajid"
+  }
+}
 
