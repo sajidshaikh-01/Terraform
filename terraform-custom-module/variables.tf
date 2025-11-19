9439851ef4b5a0683ec68ccb4d6a59cf5e575765
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-19 21:58:16 +0530

    Update variables.tf

--- terraform-custom-module/variables.tf
+++ terraform-custom-module/variables.tf
@@ -1,1 +1,38 @@
+variable "name" {
+  type        = string
+  description = "EC2 instance name"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  type        = string
+}
+
+variable "key_name" {
+  type        = string
+}
+
+variable "security_groups" {
+  type        = list(string)
+}
+
+variable "user_data" {
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
 
