a4d3789c262ecc541b458111d7d4b814a0e70566
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-19 21:58:03 +0530

    Update main.tf

--- terraform-custom-module/main.tf
+++ terraform-custom-module/main.tf
@@ -1,1 +1,16 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_groups
+  user_data              = var.user_data
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.name
+    }
+  )
+}
 
